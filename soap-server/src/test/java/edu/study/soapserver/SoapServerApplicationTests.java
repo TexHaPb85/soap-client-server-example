6cbfe2c2e981ffbff3d77d35f5bc61fdef9e0591
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2020-12-21 13:54:03 +0200

    maven multimodule project created

---
+++ soap-server/src/test/java/edu/study/soapserver/SoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.study.soapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
